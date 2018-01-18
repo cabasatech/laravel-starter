e8cf2f2e07ebd7768886f236745188e2a6f9ffec
Mayur Shingrakhiya <mayur@cabasatech.com>
2018-01-18 13:33:51 +0530

    migration file

---
+++ database/migrations/2018_01_17_123424_add_delete_field_in_task.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDeleteFieldInTask extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table(
+            'tasks', function (Blueprint $table) {
+                $table->softDeletes();
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table(
+            'tasks', function (Blueprint $table) {
+                $table->dropSoftDeletes();
+            }
+        );
+    }
+}
